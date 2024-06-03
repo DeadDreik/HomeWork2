9278488ce9edab0207650308719b1642a5fb4173
Shamil <65324697+DeadDreik@users.noreply.github.com>
2024-06-03 16:29:14 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,5 @@
+kolich_dz = 12
+kolich_time = 1.5
+name_kurs = "Python"
+time_one_task = kolich_time / kolich_dz
+print(name_kurs, ":", name_kurs, "всего задач:", kolich_dz, ",", "затрачено часов:", kolich_time, ",", "среднее время выполнения", time_one_task, "часа" )
